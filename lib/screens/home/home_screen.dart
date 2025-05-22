e2aed6a2db391b6892087fed78f5fa9649328d1f
adandoang <akhdanjzif@gmail.com>
2025-05-22 22:47:43 +0700

    membuat my bottom nav bar dan memanggil dan memanggilnya bersama body du home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:plantapp/components/my_bottom_nav_bar.dart';
+import 'package:plantapp/screens/home/components/body.dart';
 
 
 class HomeScreen extends StatelessWidget {
@@ -8,6 +10,8 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
+      body: Body(),
+      bottomNavigationBar: MyBottomNavBar(),
     );
   }
 
