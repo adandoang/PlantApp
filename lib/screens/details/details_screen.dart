f21970815a1d0fa31837c9ce489e2b13418023b5
adandoang <akhdanjzif@gmail.com>
2025-05-22 23:03:01 +0700

    membuat icon_card

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'components/body.dart';
+
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Body(),
+    );
   }
 }
